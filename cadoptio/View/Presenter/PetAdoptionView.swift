93179b9656a7453b7d8a7cd847509284e73b5ec6
aurellee <165924639+aurellee@users.noreply.github.com>
2024-04-26 16:52:17 +0700

    Update PetAdoptionView.swift

--- cadoptio/View/Presenter/PetAdoptionView.swift
+++ cadoptio/View/Presenter/PetAdoptionView.swift
@@ -13,6 +13,7 @@ struct PetAdoptionView: View {
     @State private var isFilterViewPresented = false
     @State private var isAddNewPetViewPresented = false
     @Environment(\.modelContext) private var context
+    //hohohihi
     
     /*var position: (x: Int, y: Int, z: Int) = (3, 4, 5)*/ /* ini ngasi tau tipe data mirip tuples, jadi manggilnya bisa position.x */
     
